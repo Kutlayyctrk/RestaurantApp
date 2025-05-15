e3874d4fdbcc6d6637d1895cac34f4fdf5037ccf
Kutlay Yücetürk <Kutlay Ycetrk@DESKTOP-QT0DLI7>
2025-05-15 04:17:02 +0300

    Add project files.

---
+++ RestaurantApp.DAL/RestaurantContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using RestaurantApp.Entities;
+
+namespace RestaurantApp.DAL
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext() : base("name=RestaurantDbConnectionString")
+        {
+
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+
+
+    }
+}
---
+++ RestaurantApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestaurantApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestaurantApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c459378-5bb1-4cdc-b88f-a16e8725c6c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
